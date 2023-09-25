42dffe7f9382cb82751a6870a5ea8844e609b655
lavanyasivashan <110533756+lavanyasivashan@users.noreply.github.com>
2023-09-25 12:41:10 +0530

    Add files via upload

---
+++ Day 7/src/layout/About.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import './About.css';
+import Move from './Move';
+const About = () => {
+  return (
+    <div id='hi'><h1>
+    <div className="rectangle">
+    <div className="heading">ABOUT US</div>
+    </div></h1><br/><br/><br/><br/>
+      <div className='h'>Welcome to our company's "About Us" page. Here, you can learn more about our mission. VehicleCare Reinventing the automotive space, VehicleCare is one of the fastest-growing online platforms for auto service and damage repair. Our digitized solution extends the reach of our quick and easy processes to both business and consumer markets, and with a deft management system of damage assessment and repair, our model has helped numerous car owners, fleet owners, manufacturers, insurance brokers, and insurers enhance customer satisfaction, eliminate redundancies in the time cycle, and easily compare, assess, and find the best offer on their vehicle's repair damage.</div>
+    <Move/></div>
+    
+   
+  );
+};
+
+export default About;
