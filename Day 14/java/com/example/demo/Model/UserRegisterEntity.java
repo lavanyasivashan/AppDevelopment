c427a09de9d9fa857506c47d22185261794ed72f
lavanyasivashan <110533756+lavanyasivashan@users.noreply.github.com>
2023-10-05 14:19:41 +0530

    Add files via upload

---
+++ Day 14/java/com/example/demo/Model/UserRegisterEntity.java
@@ -0,0 +1,62 @@
+package com.example.demo.Model;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="UserRegister")
+public class UserRegisterEntity {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int UserRegisterId;
+	private String fullname;
+	
+	public int getUserRegisterId() {
+		return UserRegisterId;
+	}
+	public void setUserRegisterId(int userRegisterId) {
+		UserRegisterId = userRegisterId;
+	}
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	public Long getMobile() {
+		return mobile;
+	}
+	public void setMobile(Long mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	private Long mobile;
+	private String email;
+	private String password;
+
+
+}
